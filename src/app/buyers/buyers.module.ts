b71e7fea87e2886365faa1b55979786cdc1de8b9
Muhammad Zubaid Rasool <63468411+zubaidrasool007@users.noreply.github.com>
2022-02-19 23:33:28 +0500

    Buyers component added

--- src/app/buyers/buyers.module.ts
+++ src/app/buyers/buyers.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BuyersComponent } from './buyers/buyers.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [BuyersComponent],
   imports: [
     CommonModule
   ]
